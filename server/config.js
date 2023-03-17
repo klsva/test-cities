6c6eb69c8f6646464adaa2b5ddfedacc7d273b89
Klsva <klsva.el@gmail.com>
2023-03-17 13:04:28 +0300

    nodemon config & env changes

--- server/config.js
+++ server/config.js
@@ -3,10 +3,11 @@ import dotenv from 'dotenv';
 
 const configPath = '.env';
 
-const buffer = fs.readFileSync(configPath);
-const config = dotenv.parse(buffer);
-for (const key in config) {
-  process.env[key] = config[key];
+if (process.env.NODE_ENV === 'dev'){
+  const buffer = fs.readFileSync(configPath);
+  const config = dotenv.parse(buffer);
+  for (const key in config) {
+    process.env[key] = config[key];
+  }
 }
-
 export const PORT = process.env.PORT;
